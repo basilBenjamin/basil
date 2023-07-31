5facf7d23b2a8cd6ecc22467c8a9457d692b1981
basilBenjamin <108117004+basilBenjamin@users.noreply.github.com>
2023-07-31 21:56:29 +0000

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x4316af68f6e306f98bd9925edb5ea63f79cb7149";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
